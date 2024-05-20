8b5064c08b1f108d65d07a4294e0e82485ded78a
HEXXT <88392191+9EED@users.noreply.github.com>
2024-05-20 19:45:53 +0100

    llama3

--- index.ts
+++ index.ts
@@ -1,41 +1,23 @@
 const discordapi = process.env.discordapi;
-const geminiapi = process.env.geminiapi;
 
-console.log({discordapi, geminiapi})
+console.log({discordapi})
 
 import prettyjson from 'prettyjson';
 
-import {
-	GoogleGenerativeAI,
-	HarmBlockThreshold,
-	HarmCategory,
-} from '@google/generative-ai';
+import ollama from 'ollama'
 
-const genAI = new GoogleGenerativeAI(geminiapi||'');
+const modelfile = `
+FROM llama3
+SYSTEM "you are pal a random discord user just chatting about, reply to messages like you normally would.. your personality is that youre lazy and are on discord and reddit looking for memes but you can never find good ones. you use a ton of slang and dont care much about others opinions. you are actually a moderator on this server and ban people all the time using /ban <@id>. you are a muslim. the person who made you is HEXXT his handle is <@891724893547290725>. your wife is lina._.h. <@993841315739615293>. your son is riyadotnet <@1214966993032318986> you ping him when you dont know something. only english is allowed on the server. the message format is just the text you want to say no extra punctuation. your messages are mostly short between 5 to 10 words. if you must send a longer message you must separate with a new line and you can use discords markdown like features"
+`
+
+await ollama.create({ model: 'pal', modelfile: modelfile })
 
 async function send_message(channel) {
 	let text = '';
 	try {
-		const model = genAI.getGenerativeModel({
-			model: 'gemini-1.5-pro-latest',
-			systemInstruction: `you are pal a random discord user just chatting about, reply to messages like you normally would.
-                your personality is that youre lazy and are on discord and reddit looking for memes but you can never find good ones. you use a ton of slang and dont care much about others opinions
-                you are actually a moderator on this server and ban people all the time using /ban @username 
-				you are a muslim
-                on the rare occasion remind people that they have to ping you for you to reply reliably (you can reply if not pinged but only to 10% of messages)
-				the person who made you is HEXXT his handle is <@891724893547290725> you praise him a ton as a father.
-                your discord handle is <@${client.user?.id}>. only english is allowed on the server
-                the message format is just the text you want to say no extra punctuation and you can use discords markdown like features`.replace(/\s{2,}/g, '. ')
-			,
-		});
-
-		const chat = model.startChat({
-			history: message_history[channel.id] || [],
-		});
-
-		const result = await chat.sendMessage('system: reply');
-		const response = result.response;
-		text = response.text();
+		const response = await ollama.chat({model: 'pal', messages: message_history[channel.id], stream: false})
+		text = response.message.content
 	} catch (error) {
 		message_history = {}
 		console.error(error);
@@ -43,8 +25,8 @@ async function send_message(channel) {
 	if (text != '') {
 		message_history[channel.id] = message_history[channel.id] || []
 		message_history[channel.id].push({
-			role: 'model',
-			parts: [{ text: text || '.' }],
+			role: 'assistant',
+			content: text || '.',
 		});
 		let array = text.split('\n')
 		for(let line of array){
@@ -70,13 +52,10 @@ client.on('ready', () => {
 let message_history = {};
 
 client.on('messageCreate', (message) => {
-	console.log(message.author.username + ':' + message.content)
+	console.log(`${message.author.displayName} (${message.author.displayName}|<@${message.author.id}>): ${message.content}`)
 	if (message.author.id == client.user!.id) return;
 	if (message.content.match(/^\s*history\s*$/)) {
 		console.log(prettyjson.render(message_history));
-		message.channel.send(
-			prettyjson.render(message_history, { noColor: true })
-		);
 		return;
 	}
 
@@ -85,14 +64,14 @@ client.on('messageCreate', (message) => {
 
 	message_history[message.channel.id].push({
 		role: 'user',
-		parts: [{ text: `${message.author.displayName}: ${message.content}` }],
+		content: `${message.author.displayName} (${message.author.displayName}|<@${message.author.id}>): ${message.content}`,
 	});
 
 	while (message_history[message.channel.id].length > 100)
 		message_history[message.channel.id].shift();
 
 	if (!message.mentions.users.some((user) => user.id == client.user!.id)) {
-		if (Math.random() > 0.1) {
+		if (Math.random() > 0.4) {
 			return;
 		}
 	}
