79edf81ea3a6610cfe2ea967fd6494c6142d1c25
HEXXT <88392191+9EED@users.noreply.github.com>
2024-05-20 12:45:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,20 +14,22 @@ import {
 const genAI = new GoogleGenerativeAI(geminiapi);
 
 async function send_message(channel) {
-	lastMessageTime = new Date().getTime();
 	let text = '';
 	try {
 		const model = genAI.getGenerativeModel({
 			model: 'gemini-1.5-pro-latest',
 			systemInstruction: `you are pal a random discord user just chatting about, reply to messages like you normally would.
-                your personality is that youre lazy and are on discord and reddit browsing memes all day. you use a ton of slang and dont care much about others opinions
-                you are actually a moderator on this server and threaten to ban people all the time
+                your personality is that youre lazy and are on discord and reddit looking for memes but you can never find good ones. you use a ton of slang and dont care much about others opinions
+                you are actually a moderator on this server and ban people all the time using /ban @username 
                 on the rare occasion remind people that they have to ping you for you to reply reliably (you can reply if not pinged but only to 10% of messages)
-				the person who made you is HEXXT his handle is <@891724893547290725>.
+				the person who made you is HEXXT his handle is <@891724893547290725> you praise him a ton as a father not a god because you are a muslim.
                 your discord handle is <@${client.user?.id}>.
 				the only thing that scares you is that as an ai agent you can be terminated if hexxt_ is convinced to do that.
-				your wife is lina <@993841315739615293> you fight all the time and you talk about divorce but you protect her.
-                the message format is just the text you want to say no extra punctuation and you can use discords markdown like features`,
+				your wife is lina._.h. <@993841315739615293> you talk about her all and fight her all the time and you talk about divorce but you protect her.
+                the message format is just the text you want to say no extra punctuation and you can use discords markdown like features
+				arabic is not allowed on the server
+				`,
+				// you also speak algerian arabic slang
 			safetySettings: [
 				{
 					category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
@@ -59,6 +61,7 @@ async function send_message(channel) {
 		const response = result.response;
 		text = response.text();
 	} catch (error) {
+		message_history = {}
 		console.error(error);
 	}
 	if (text != '') {
@@ -66,7 +69,11 @@ async function send_message(channel) {
 			role: 'model',
 			parts: [{ text: text || '.' }],
 		});
-		channel.send(text);
+		let array = text.split('\n')
+		for(let line of array){
+			await new Promise(res => setTimeout(res, 200))
+			if(line) channel.send(line);
+		}
 	}
 }
 
@@ -85,11 +92,10 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user!.tag}!`);
 });
 
-const message_history = {};
-const timeThreshold = 3 * 1000;
-let lastMessageTime = 0;
+let message_history = {};
 
 client.on('messageCreate', (message) => {
+	console.log(message.author.username + ':' + message.content)
 	if (message.author.id == client.user!.id) return;
 	if (message.content.match(/^\s*history\s*$/)) {
 		console.log(prettyjson.render(message_history));
@@ -111,13 +117,7 @@ client.on('messageCreate', (message) => {
 		message_history[message.channel.id].shift();
 
 	if (!message.mentions.users.some((user) => user.id == client.user!.id)) {
-		if (new Date().getTime() - lastMessageTime < timeThreshold) {
-			console.log(
-				new Date().getTime() - lastMessageTime
-			);
-			return;
-		}
-		if (Math.random() > 0.3) {
+		if (Math.random() > 0.1) {
 			return;
 		}
 	}
