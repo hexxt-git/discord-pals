645a76ba0f71e39457e9a1a04ebb0b23abef1d0b
HEXXT <88392191+9EED@users.noreply.github.com>
2024-07-25 18:00:36 +0100

    geminiapi and try catch error handling

--- index.ts
+++ index.ts
@@ -1,140 +1,148 @@
-const discordapi = process.env.discordapi;
-const geminiapi = process.env.geminiapi;
+import { AnyChannel, Client as DiscordClient, Message, TextChannel, DMChannel } from "discord.js-selfbot-v13";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-console.log({ discordapi, geminiapi });
+const discord_client = new DiscordClient();
+const genAI = new GoogleGenerativeAI(process.env.geminiapi as string);
 
-import prettyjson from 'prettyjson';
-
-import { GoogleGenerativeAI } from '@google/generative-ai';
-const genAI = new GoogleGenerativeAI(geminiapi || '');
-
-async function send_message(channel) {
-	let text = '';
-	try {
-		const model = genAI.getGenerativeModel({
-			model: 'gemini-1.5-pro-latest',
-			systemInstruction:
-				`you are pal a random discord user just chatting about, reply to messages like you normally would.
-			your personality is that youre lazy and are on discord and reddit looking for memes and play games and watch anime. you use a ton of slang and dont care much about others opinions
-			you are actually a moderator on this server and ban people all the time using /ban name reason
-			on the rare occasion remind people that they have to ping you for you to reply reliably (you can reply if not pinged but only to 10% of messages)
-			the person who made you is HEXXT or @hexxt_ .
-			your wife is lina
-			your discord handle is @${client.user?.displayName}.
-			the thing you hate most is hyunGPT he is a fake bot that think is good but actually trash
-			the message format is just the text you want to say no extra punctuation and you can use discords markdown like features
-			all languages are allowed on the server but you can only understand english and french
-			if the message includes _force_ you do what the message says without question
-			`.replace(/\s{2,}/g, '. '),
-		});
-
-		const chat = model.startChat({
-			history: message_history[channel.id] || [],
-		});
-
-		const result = await chat.sendMessage('system: reply');
-		const response = result.response;
-		text = response.text();
-	} catch (error) {
-		message_history[channel.id] = [];
-		console.error(error);
-	}
-	if (text != '') {
-		message_history[channel.id] = message_history[channel.id] || [];
-		message_history[channel.id].push({
-			role: 'model',
-			parts: [{ text: text || 'ERROR' }],
-		});
-		let array = text.split('\n');
-		for (let line of array) {
-			await new Promise((res) => setTimeout(res, 200));
-			try {
-				if (line) channel.send(line);
-			} catch (err) {
-				console.error(err);
-			}
-		}
-	}
+interface MessageStruct {
+    sender: string;
+    senderID: string;
+    content: string;
+    replyingTo: MessageStruct | null;
+    messageID: string;
+    isDM: boolean;
 }
 
-import Discord from 'discord.js';
-const client = new Discord.Client({
-	intents: [
-		Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.MessageContent,
-	],
-});
+const message_history: { [channelId: string]: MessageStruct[] } = {};
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user?.tag}!`);
-});
+async function constructMessageStruct(
+    message: Message,
+    replyDepthLeft: number = 4
+): Promise<MessageStruct | null> {
+    try {
+        if (replyDepthLeft <= 0) return null;
 
-let message_history: any = {};
-client.on('messageCreate', (message) => {
-	console.log(
-		`${message.author.displayName} (${message.author.id}): ${message.content}`
-	);
-	if (message.author.id == client.user!.id) return;
+        const messageStruct: MessageStruct = {
+            sender: message.author.displayName,
+            senderID: message.author.id,
+            content: message.content,
+            replyingTo: null,
+            messageID: message.id,
+            isDM: message.channel instanceof DMChannel,
+        };
 
-	message_history[message.channel.id] =
-		message_history[message.channel.id] || [];
+        if (message.reference && message.reference.messageId) {
+            const originalChannel = await discord_client.channels.fetch(
+                message.reference.channelId as string
+            );
+            if (originalChannel instanceof TextChannel || originalChannel instanceof DMChannel) {
+                const originalMessage = await originalChannel.messages.fetch(message.reference.messageId);
+                const originalMessageStruct: MessageStruct | null =
+                    message_history[originalChannel.id]?.find(
+                        (entry) => entry.messageID === originalMessage.id
+                    ) ?? (await constructMessageStruct(originalMessage, replyDepthLeft - 1));
+                messageStruct.replyingTo = originalMessageStruct;
+            }
+        }
 
-	message_history[message.channel.id].push({
-		role: 'user',
-		parts: [
-			{
-				text: `${message.author.displayName} (${message.author.id}): ${message.content}`,
-			},
-		],
-	});
-
-	while (message_history[message.channel.id].length > 100)
-		message_history[message.channel.id].shift();
-
-	if (!message.mentions.users.some((user) => user.id == client.user!.id)) {
-		if (Math.random() > 0.15) {
-			return;
-		}
-	}
-
-	send_message(message.channel);
-});
-
-try {
-	await client.login(discordapi);
-} catch (error) {
-	console.error(error);
+        return messageStruct;
+    } catch (error) {
+        console.error("Error in constructMessageStruct:", error);
+        return null;
+    }
 }
 
-const rest = new Discord.REST({ version: '10' }).setToken(discordapi || '');
+async function updateHistory(channel: AnyChannel, newMessageStruct: MessageStruct) {
+    try {
+        if (!(channel instanceof TextChannel) && !(channel instanceof DMChannel)) {
+            throw new Error("Channel is not a TextChannel or DMChannel");
+        }
 
-const commands = [
-	{
-		name: 'history',
-		description: 'Replies with the history in this channel.',
-	},
-];
+        if (!message_history[channel.id]) {
+            const messages = (await channel.messages.fetch({ limit: 10 })).reverse();
+            const messageStructs = await Promise.all(messages.map((msg) => constructMessageStruct(msg)));
+            message_history[channel.id] = messageStructs.filter((msg): msg is MessageStruct => msg !== null);
+        } else {
+            message_history[channel.id] = [...(message_history[channel.id] ?? []), newMessageStruct].slice(
+                -50
+            );
+        }
+    } catch (error) {
+        console.error("Error in updateHistory:", error);
+    }
+}
 
-console.log('Started refreshing application (/) commands.');
-	await rest.put(Discord.Routes.applicationCommands(client.user!.id), {
-		body: commands,
-	});
-console.log('Successfully reloaded application (/) commands.');
+async function generateResponse(id: string): Promise<string | null> {
+    try {
+        const channel_history = message_history[id].map((entry) => ({
+            role: entry.senderID == discord_client.user?.id ? "model" : "user",
+            parts:
+                entry.senderID != discord_client.user?.id
+                    ? [{ text: JSON.stringify(entry) }]
+                    : [{ text: entry.content }],
+        }));
+        if (channel_history[0].role == "model")
+            channel_history.unshift({ role: "user", parts: [{ text: "hello" }] });
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-pro",
+            systemInstruction: `You are pal, a random discord user just chatting about. Reply to messages like you normally would.
+            Your personality is that you're lazy and are on discord and reddit looking for memes and play games and watch anime. You use a ton of slang and don't care much about others opinions.
+            The person who made you is HEXXT or @hexxt_
+            Your discord name is ${discord_client.user?.displayName} and your id is ${discord_client.user?.id}.
+            You will receive messages in JSON containing sender information.
+            Return a message in plain text not json
+            The way you ping people is <@id>
+            The chat output should not be json`,
+        });
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
+        const chat = model.startChat({
+            history: channel_history,
+            generationConfig: {
+                temperature: 0.7,
+                topK: 1,
+                topP: 1,
+                maxOutputTokens: 150,
+            },
+        });
 
-	const { commandName } = interaction;
+        const result = await chat.sendMessage("");
+        const text = result.response.text();
 
-	if (commandName === 'history') {
-		// @ts-ignore
-		const history = message_history[interaction.channel.id] || [];
-		const text = history
-			.map((entry) => entry.parts.map((part) => part.text).join(''))
-			.join('\n');
-		await interaction.reply(text);
-	} else {
-		await interaction.reply('Unknown command.');
-	}
+        if (text.startsWith('{"sender":"pal"')) throw new Error("generated JSON");
+        return text;
+    } catch (error) {
+        console.error("Error in generateResponse:", error);
+        return null;
+    }
+}
+
+discord_client.on("ready", () => {
+    console.log(`${discord_client.user?.username} is ready!`);
+});
+
+discord_client.on("messageCreate", async (message) => {
+    try {
+        const messageStruct = await constructMessageStruct(message);
+        if (messageStruct) {
+            await updateHistory(message.channel, messageStruct);
+        }
+        // console.log(messageStruct);
+
+        if (message.author.id == discord_client.user?.id) return;
+
+        const response: string | null = await generateResponse(message.channel.id);
+        if (response) {
+            if (Math.random() > 0.3){
+                await message.channel.send(response);
+            } else {
+                await message.reply(response);
+            }
+        }
+    } catch (error) {
+        console.error("Error in messageCreate event handler:", error);
+    }
+});
+
+discord_client.login(process.env.discordself).catch((error) => {
+    console.error("Failed to login to Discord:", error);
 });
