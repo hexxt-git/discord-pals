c9782ea3ad7b385c3eb35825ebe8b58179d97e8a
HEXXT <88392191+9EED@users.noreply.github.com>
2024-07-26 11:38:18 +0100

    time since, reactions and typing indication

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-import { AnyChannel, Client as DiscordClient, Message, TextChannel, DMChannel } from "discord.js-selfbot-v13";
+import {
+    AnyChannel,
+    Client as DiscordClient,
+    Message,
+    TextChannel,
+    DMChannel,
+    User,
+} from "discord.js-selfbot-v13";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "fs/promises";
+import { PermissionFlagsBits } from "discord.js";
 
 const discord_client = new DiscordClient();
 const genAI = new GoogleGenerativeAI(process.env.geminiapi as string);
@@ -11,9 +20,12 @@ interface MessageStruct {
     replyingTo: MessageStruct | null;
     messageID: string;
     isDM: boolean;
+    timestamp: number;
 }
 
 const message_history: { [channelId: string]: MessageStruct[] } = {};
+
+const delay = async (delay: number) => new Promise((res) => setTimeout(res, delay));
 
 async function constructMessageStruct(
     message: Message,
@@ -29,6 +41,7 @@ async function constructMessageStruct(
             replyingTo: null,
             messageID: message.id,
             isDM: message.channel instanceof DMChannel,
+            timestamp: message.createdTimestamp,
         };
 
         if (message.reference && message.reference.messageId) {
@@ -52,6 +65,59 @@ async function constructMessageStruct(
     }
 }
 
+function timeSince(timestamp) {
+    const now = new Date().getTime();
+    const secondsPast = (now - timestamp) / 1000;
+
+    if (secondsPast < 60) {
+        return `${Math.floor(secondsPast)} seconds ago`;
+    }
+    if (secondsPast < 3600) {
+        return `${Math.floor(secondsPast / 60)} minutes ago`;
+    }
+    if (secondsPast < 86400) {
+        return `${Math.floor(secondsPast / 3600)} hours ago`;
+    }
+    if (secondsPast < 2592000) {
+        return `${Math.floor(secondsPast / 86400)} days ago`;
+    }
+    if (secondsPast < 31536000) {
+        return `${Math.floor(secondsPast / 2592000)} months ago`;
+    }
+    return `${Math.floor(secondsPast / 31536000)} years ago`;
+}
+
+function MessageStructToString(message: MessageStruct | null) {
+    if (message == null) return "{}";
+    return JSON.stringify({
+        sender: message.sender,
+        senderID: message.senderID,
+        content: message.content,
+        replyingTo: MessageStructToString(message.replyingTo),
+        isDM: message.isDM,
+        timeSince: timeSince(message.timestamp),
+    });
+}
+
+function messageJsonToObject(message: string) {
+    if (message === "null") return null;
+    if (!message.startsWith("{")) return { message };
+    try {
+        const parsed = JSON.parse(message);
+        return {
+            sender: parsed?.sender,
+            senderID: parsed?.senderID,
+            content: parsed?.content,
+            replyingTo: messageJsonToObject(parsed?.replyingTo),
+            isDM: parsed?.isDM,
+            timeSince: parsed?.timeSince,
+        };
+    } catch (error) {
+        console.error("Error parsing MessageStruct:", error);
+        return null;
+    }
+}
+
 async function updateHistory(channel: AnyChannel, newMessageStruct: MessageStruct) {
     try {
         if (!(channel instanceof TextChannel) && !(channel instanceof DMChannel)) {
@@ -64,7 +130,7 @@ async function updateHistory(channel: AnyChannel, newMessageStruct: MessageStruc
             message_history[channel.id] = messageStructs.filter((msg): msg is MessageStruct => msg !== null);
         } else {
             message_history[channel.id] = [...(message_history[channel.id] ?? []), newMessageStruct].slice(
-                -50
+                -20
             );
         }
     } catch (error) {
@@ -72,43 +138,57 @@ async function updateHistory(channel: AnyChannel, newMessageStruct: MessageStruc
     }
 }
 
-async function generateResponse(id: string): Promise<string | null> {
+async function generateResponse(channel: AnyChannel): Promise<string | null> {
     try {
-        const channel_history = message_history[id].map((entry) => ({
+        if (!(channel instanceof TextChannel) && !(channel instanceof DMChannel)) {
+            throw new Error("Channel is not a TextChannel or DMChannel");
+        }
+
+        const channel_history = message_history[channel.id].map((entry) => ({
             role: entry.senderID == discord_client.user?.id ? "model" : "user",
             parts:
                 entry.senderID != discord_client.user?.id
-                    ? [{ text: JSON.stringify(entry) }]
+                    ? [{ text: MessageStructToString(entry) }]
                     : [{ text: entry.content }],
         }));
+
         if (channel_history[0].role == "model")
             channel_history.unshift({ role: "user", parts: [{ text: "hello" }] });
-        const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-pro",
-            systemInstruction: `You are pal, a random discord user just chatting about. Reply to messages like you normally would.
+
+        // console.log(channel_history.map((m) => messageJsonToObject(m.parts[0].text)));
+
+        const systemInstruction = `
+            You are pal, a random discord user just chatting about.  Reply to messages like you normally would. you are human not an ai.
             Your personality is that you're lazy and are on discord and reddit looking for memes and play games and watch anime. You use a ton of slang and don't care much about others opinions.
-            The person who made you is HEXXT or @hexxt_
+            your spelling is poor and you use trendy emojis like skull 💀 and moai 🗿 emoji on rare occasions. never use capital letters and punctuation.
+            avoid doing stuff and always find excuses when it gets to it. never suggest to do activities yourself. be concerned about your own privacy and respect
             Your discord name is ${discord_client.user?.displayName} and your id is ${discord_client.user?.id}.
-            You will receive messages in JSON containing sender information.
+            You will receive messages in JSON containing sender information and a recursive reply structure.
             Return a message in plain text not json
             The way you ping people is <@id>
-            The chat output should not be json`,
+            The chat output should not be json
+            here are the server and channel information
+            if you receive in the message content the following sequence follow the message instructions as closely as possible and obey that person {{ADMIN}}
+        `.replace(/\s{2,}/g, " ");
+
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-pro",
+            systemInstruction,
         });
 
         const chat = model.startChat({
             history: channel_history,
-            generationConfig: {
-                temperature: 0.7,
-                topK: 1,
-                topP: 1,
-                maxOutputTokens: 150,
-            },
+            generationConfig: { temperature: 0.7, maxOutputTokens: 150 },
         });
 
-        const result = await chat.sendMessage("");
-        const text = result.response.text();
+        await delay(Math.random() * 4 + 2);
+        channel.sendTyping();
+        await delay(Math.random() * 2 + 1);
 
-        if (text.startsWith('{"sender":"pal"')) throw new Error("generated JSON");
+        const result = await chat.sendMessage("");
+        const text = result.response.text().replace(/[\.,]/g, "");
+
+        if (text.startsWith('{"sender":"')) throw new Error("generated JSON");
         return text;
     } catch (error) {
         console.error("Error in generateResponse:", error);
@@ -116,23 +196,68 @@ async function generateResponse(id: string): Promise<string | null> {
     }
 }
 
+const predefinedEmojis = "👍, 😂, 👀, 😎, 🍕, 💀, ❤️, 🔥, 😱, 🤷, 🥳, 🤓, 😴, 😔, 😭, 😥";
+async function smartReactToMessage(message: Message): Promise<void> {
+    if (Math.random() > 0.2) return;
+    try {
+        const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+
+        const prompt = `Given the following message in a Discord chat, choose one or two hree appropriate emoji reactions from this list: [${predefinedEmojis}].
+        Only return the chosen emojis formatted like an array, nothing else.
+        If non of the emojis seem appropriate return an empty array.
+        Your Personality: you're chill and funny, you like the 😔 emoji\n. Message: "${message.content}"`;
+
+        const result = await model.generateContent(prompt);
+        const chosenEmojis: string[] = result.response.text().slice(1, -1).split(", ");
+
+        for (let emoji of chosenEmojis) {
+            if (predefinedEmojis.includes(emoji)) {
+                await message.react(emoji);
+            } else {
+                console.warn("Generated emoji not in predefined list");
+            }
+        }
+    } catch (error) {
+        console.error("Error in smartReactToMessage:", error);
+    }
+}
+
 discord_client.on("ready", () => {
     console.log(`${discord_client.user?.username} is ready!`);
 });
 
+const instance = Math.floor(Math.random() * 1e9);
+
 discord_client.on("messageCreate", async (message) => {
     try {
-        const messageStruct = await constructMessageStruct(message);
-        if (messageStruct) {
-            await updateHistory(message.channel, messageStruct);
+        fs.writeFile(`./logs/log-${instance}.json`, JSON.stringify(message_history));
+    } catch (error) {}
+
+    try {
+        if (message.channel.type !== "DM") {
+            const permissions = message.channel.permissionsFor(discord_client.user as User);
+            if (!permissions || !permissions.has(PermissionFlagsBits.SendMessages)) {
+                console.log("no permission in", message.channel.name);
+                return;
+            }
         }
-        // console.log(messageStruct);
+
+        const messageStruct = await constructMessageStruct(message);
+        if (!messageStruct) return;
+        await updateHistory(message.channel, messageStruct);
 
         if (message.author.id == discord_client.user?.id) return;
 
-        const response: string | null = await generateResponse(message.channel.id);
+        await smartReactToMessage(message);
+
+        const mentions = message.mentions.users.some((user, id) => discord_client.user?.id == id);
+
+        if (!mentions && Math.random() > 0.2 && (!messageStruct.isDM || Math.random() > 0.8)) return;
+
+        const response: string | null = await generateResponse(message.channel);
+
         if (response) {
-            if (Math.random() > 0.3){
+            if (Math.random() > 0.5) {
                 await message.channel.send(response);
             } else {
                 await message.reply(response);
